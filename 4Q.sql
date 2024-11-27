bc26576e48472d08d5a209be1eacd261e9d064b8
Rajashekar <rajashekar.battula.b@gmail.com>
2024-11-28 00:42:35 +0530

    Delete 4Q.sql

--- 4Q.sql
+++
@@ -1,7 +0,0 @@
--- Company Size & Layoffs: 
-select * from `tech layoffs`;
-
-select company, sum(Company_Size_before_Layoffs) as Total_Company_size, sum(Laid_Off) as Lay_offs from `tech layoffs`
-group by company;
-
-select * from `tech layoffs` where company = "zulily";
