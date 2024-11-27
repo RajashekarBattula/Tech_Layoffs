32163305c3fcba715ed5a656e259bc189ab586a3
Rajashekar <rajashekar.battula.b@gmail.com>
2024-11-28 00:42:44 +0530

    Delete 5Q.sql

--- 5Q.sql
+++
@@ -1,6 +0,0 @@
--- relation between Financial Health and lays_offs
-
-select * from `tech layoffs`;
-
-select company, sum(Money_Raised_in_$_mil) as Financial_health, sum(Laid_Off) as Total_lay_offs 
-from `tech layoffs` group by company order by Financial_health ;
